a4e41f155534807e3bdb57622d61911e924b4ce9
Luiz Leme <lapaesleme@ic.uff.br>
2021-02-17 14:59:31 -0300

    Updates.

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/exercicio11/HenriqueDeMoraisPorto.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uff.ic.lleme.tcc00328.s20202.exercicios.exercicio11;
+
+public class HenriqueDeMoraisPorto {
+
+    public static void main(String[] args) {
+        int[] vet = new int[50];
+        int contador = 0;
+        int i = 0;
+        while (contador != 50) {
+            if (primo(i)) {
+                vet[contador] = i;
+                contador++;
+            }
+            i++;
+        }
+        for (int b = 0; b <= 49; b++)
+            System.out.println(vet[b]);
+
+    }
+
+    public static boolean primo(int n) {
+        if (n != 0 && n != 1) {
+            int x = 2;
+            while (n > x) {
+                if (n % x == 0)
+                    return false;
+                x++;
+            }
+            return true;
+        } else
+            return false;
+    }
+
+}
---
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/exercicio2/HenriqueDeMoraisPorto.java
@@ -0,0 +1,19 @@
+package uff.ic.lleme.tcc00328.s20202.exercicios.exercicio2;
+
+import java.util.Scanner;
+
+public class HenriqueDeMoraisPorto {
+
+    public static void main(String[] args) {
+        Scanner ola = new Scanner(System.in);
+        int n = ola.nextInt();
+        int temp = 1;
+
+        int a = 1;
+        while (n > a) {
+            a++;
+            temp = temp * a;
+        }
+        System.out.println(temp);
+    }
+}
