7ae2f7aed7a921769ff4f520fa55c6a82361f9cf
Henrique Porto <henriqueporto@id.uff.br>
2021-02-17 17:13:26 -0300

    Correção exercício 11 - 17/02/2021

--- src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/exercicio11/HenriqueDeMoraisPorto.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/exercicio11/HenriqueDeMoraisPorto.java
@@ -5,35 +5,42 @@
  */
 package uff.ic.lleme.tcc00328.s20202.exercicios.exercicio11;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+
 public class HenriqueDeMoraisPorto {
-
-    public static void main(String[] args) {
-        int[] vet = new int[50];
-        int contador = 0;
-        int i = 0;
-        while (contador != 50) {
-            if (primo(i)) {
-                vet[contador] = i;
-                contador++;
+    public static void main(String[] args){
+        int i=1;
+        long a=1;
+        while(i<=50){
+            if(primo(i)){
+                a = a * i;
             }
-            i++;
+            
+            i++; 
         }
-        for (int b = 0; b <= 49; b++)
-            System.out.println(vet[b]);
-
+        System.out.println(a);
+        
+        
     }
-
-    public static boolean primo(int n) {
-        if (n != 0 && n != 1) {
-            int x = 2;
-            while (n > x) {
-                if (n % x == 0)
+    public static boolean primo(int n){
+        if(n!= 0 && n!=1){
+            int x=2;
+            while(n>x){
+                if (n%x == 0){
                     return false;
+                }
                 x++;
             }
             return true;
-        } else
+        }
+        else{
             return false;
-    }
-
+        }
+    } 
+    
 }
