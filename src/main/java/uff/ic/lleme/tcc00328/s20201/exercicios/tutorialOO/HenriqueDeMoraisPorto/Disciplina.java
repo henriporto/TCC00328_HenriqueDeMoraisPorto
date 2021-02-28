e229923372328cd7f33c0c4573a5ff6d6331bf54
Henrique Porto <henriqueporto@id.uff.br>
2021-02-28 20:49:08 -0300

    Exercício 17: tutorial OO

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20201/exercicios/tutorialOO/HenriqueDeMoraisPorto/Disciplina.java
@@ -0,0 +1,37 @@
+package uff.ic.lleme.tcc00328.s20201.exercicios.tutorialOO.HenriqueDeMoraisPorto;
+public class Disciplina {
+    private String codigo, nome, sigla;
+    public Disciplina(String codigo, String sigla, String nome){
+        this.codigo=codigo;
+        this.nome=nome;
+        this.sigla=sigla;
+    }
+    public void print(){
+        System.out.println("Codigo: "+codigo+" Sigla: "+sigla+" Nome: "+nome);
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+    
+}
