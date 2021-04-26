b5eacb0cf3a1db91e3749ef5bde21ca2bd985b46
Henrique Porto <henriqueporto@id.uff.br>
2021-04-26 13:01:31 -0300

    p2
    p2

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/prova/p2/HenriqueDeMoraisPorto/Q1/Dicionario.java
@@ -0,0 +1,11 @@
+package uff.ic.lleme.tcc00328.s20202.provas.p2.HenriqueDeMoraisPorto.Q1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dicionario {
+    Map<Palavra, String> dicionario = new HashMap<Palavra, String>();
+    public Dicionario(){
+        
+    }
+}
---
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/prova/p2/HenriqueDeMoraisPorto/Q1/Palavra.java
@@ -0,0 +1,23 @@
+package uff.ic.lleme.tcc00328.s20202.provas.p2.HenriqueDeMoraisPorto.Q1;
+
+import java.util.ArrayList;
+
+public class Palavra {
+    public String fonetica;
+    public String grafia;
+    public ArrayList<Sinonimos> sinonimos = new ArrayList<>();
+    
+    public Palavra(){
+        
+    }
+    public Palavra(String grafia, String fonetica){
+        this.fonetica=fonetica;
+        this.grafia=grafia;
+        
+    }     
+    
+    
+    
+    
+    
+}
